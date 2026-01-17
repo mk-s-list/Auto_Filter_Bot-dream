e44382c61677094c748d3b9b7f3f3d782d4b32a7
mk-s-list <andsomeone58@gmail.com>
2026-01-17 10:38:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,8 +61,8 @@ auth_channels     = environ.get("AUTH_CHANNELS", "-100")# Channels for force sub
 # ============================
 # Payment Configuration
 # ============================
-QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/e419f801841c2ee3db0fc.jpg')    # QR code image for payments
-OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'ɴᴏ ᴀᴠᴀɪʟᴀʙʟᴇ ʀɪɢʜᴛ ɴᴏᴡ')    # Owner UPI ID for payments
+QR_CODE = environ.get('QR_CODE', 'https://')    # QR code image for payments
+OWNER_UPI_ID = environ.get('OWNER_UPI_ID', '@otcoffix')    # Owner UPI ID for payments
 
 STAR_PREMIUM_PLANS = {
     10: "7day",
